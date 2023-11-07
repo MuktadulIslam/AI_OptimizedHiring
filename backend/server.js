ae03eb19498b3be5d7cce20670c14e56ec572bde
Muktadul Islam <76801029+MuktadulIslam@users.noreply.github.com>
2023-11-07 16:07:27 +0600

    ADD: function which can read contract info from resume

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const pdf = require('pdf-parse');
 const natural = require('natural');
 const cors = require('cors');
+const { getInformationFromPDF } = require('./pdf_reader');
 const app = express();
 const port = 5000;
 
@@ -24,8 +25,10 @@ app.post('/upload', upload.single('pdf'), async (req, res) => {
 
     const text = data.text;
 
+    getInformationFromPDF(buffer)
+
     /* ###############We will work though this "text"####################### */
-    console.log(text)
+    // console.log(text)
 
     // Tokenization
     const tokenizer = new natural.WordTokenizer();
